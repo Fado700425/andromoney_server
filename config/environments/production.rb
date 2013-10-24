6336b932c3210437b22861aea4110829d7c420ac
Steven Ko <chunyuko85@gmail.com>
2013-10-24 11:41:21 +0800

    change log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ AndromoneyServer::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  # config.log_level = :info
+  config.active_support.deprecation = :log
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
