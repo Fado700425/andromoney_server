5879330e981a175d4dc8a3e27e46368bb61053fd
Steven Ko <chunyuko85@gmail.com>
2013-10-24 14:35:52 +0800

    isert at once

--- app/controllers/api/v1/add_datas_controller.rb
+++ app/controllers/api/v1/add_datas_controller.rb
@@ -22,79 +22,85 @@ class Api::V1::AddDatasController < ApplicationController
   private
 
   def create_all_data(user,params)
+
+    begin
+      ActiveRecord::Base.transaction do
     
-    if params[:records]
-      params[:records].each do |record_param|
-        record = Record.new(record_param)
-        record.user = user
-        record.save
-      end
-    end
+        if params[:records]
+          params[:records].each do |record_param|
+            record = Record.new(record_param)
+            record.user = user
+            record.save
+          end
+        end
 
-    if params[:category_table]
-      params[:category_table].each do |cat_param|
-        cat_param.delete :update_time
-        
-        category = Category.new(cat_param)
-        category.user = user
-        category.save
-      end
-    end
+        if params[:category_table]
+          params[:category_table].each do |cat_param|
+            cat_param.delete :update_time
+            cat_param[:user_id] = user.id
+          end
+          category = Category.create(params[:category_table])
+        end
 
-    if params[:payees]
-      params[:payees].each do |payee_param|
-        payee = Payee.new(payee_param)
-        payee.user = user
-        payee.save
-      end
-    end
+        if params[:payees]
+          params[:payees].each do |payee_param|
+            payee = Payee.new(payee_param)
+            payee.user = user
+            payee.save
+          end
+        end
 
-    if params[:currencies]
-      params[:currencies].each do |currency_param|
-        currency = Currency.new(currency_param)
-        currency.user = user
-        currency.save
-      end
-    end
+        if params[:currencies]
+          params[:currencies].each do |currency_param|
+            currency = Currency.new(currency_param)
+            currency.user = user
+            currency.save
+          end
+        end
 
-    if params[:payments]
-      params[:payments].each do |payment_param|
-        payment = Payment.new(payment_param)
-        payment.user = user
-        payment.save
-      end
-    end
+        if params[:payments]
+          params[:payments].each do |payment_param|
+            payment = Payment.new(payment_param)
+            payment.user = user
+            payment.save
+          end
+        end
 
-    if params[:periods]
-      params[:periods].each do |period_param|
-        period = Period.new(period_param)
-        period.user = user
-        period.save
-      end
-    end
+        if params[:periods]
+          params[:periods].each do |period_param|
+            period = Period.new(period_param)
+            period.user = user
+            period.save
+          end
+        end
 
-    if params[:prefs]
-      params[:prefs].each do |pref_param|
-        pref = Pref.new(pref_param)
-        pref.user = user
-        pref.save
-      end
-    end
+        if params[:prefs]
+          params[:prefs].each do |pref_param|
+            pref = Pref.new(pref_param)
+            pref.user = user
+            pref.save
+          end
+        end
 
-    if params[:projects]
-      params[:projects].each do |project_param|
-        project = Project.new(project_param)
-        project.user = user
-        project.save
-      end
-    end
+        if params[:projects]
+          params[:projects].each do |project_param|
+            project = Project.new(project_param)
+            project.user = user
+            project.save
+          end
+        end
 
-    if params[:subcategories]
-      params[:subcategories].each do |subcategory_param|
-        subcategory = Subcategory.new(subcategory_param)
-        subcategory.user = user
-        subcategory.save
+        if params[:subcategories]
+          params[:subcategories].each do |subcategory_param|
+            subcategory = Subcategory.new(subcategory_param)
+            subcategory.user = user
+            subcategory.save
+          end
+        end
       end
+    rescue
+      render :status=>404, :json=>{:message=>"Create Fail"}
+      return
     end
 
   end
