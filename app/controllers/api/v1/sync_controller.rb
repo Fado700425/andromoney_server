aa91357ffae185bfbade2b58c04d586dabc79b86
Steven Ko <chunyuko85@gmail.com>
2013-11-20 13:03:34 +0800

    change time format

--- app/controllers/api/v1/sync_controller.rb
+++ app/controllers/api/v1/sync_controller.rb
@@ -7,8 +7,8 @@ class Api::V1::SyncController < Api::V1::ApiController
     device = Device.find_by(user_id: user.id, uuid: params[:device]) if user
     
     if (user && device)
-      sync_time = Time.now.utc
-      render :status=>200, :json=>{:message=>"Sync Start Success", :sync_start_time => sync_time}
+      sync_time = DateTime.now.utc
+      render :status=>200, :json=>{:message=>"Sync Start Success", :sync_start_time => sync_time.strftime("%Y-%m-%d %H:%M:%S")}
     else
       render :status=>404, :json=>{:message=>"Start Fail, make sure db has user and device"}
     end
