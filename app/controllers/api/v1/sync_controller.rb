70321d4b0a8ada0bc032a78921eb002d08858054
Steven Ko <chunyuko85@gmail.com>
2013-11-20 09:42:18 +0800

    utc time

--- app/controllers/api/v1/sync_controller.rb
+++ app/controllers/api/v1/sync_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::SyncController < Api::V1::ApiController
     device = Device.find_by(user_id: user.id, uuid: params[:device]) if user
     
     if (user && device)
-      render :status=>200, :json=>{:message=>"Sync Start Success", :sync_start_time => Time.now}
+      render :status=>200, :json=>{:message=>"Sync Start Success", :sync_start_time => Time.now.utc}
     else
       render :status=>404, :json=>{:message=>"Start Fail, make sure db has user and device"}
     end
