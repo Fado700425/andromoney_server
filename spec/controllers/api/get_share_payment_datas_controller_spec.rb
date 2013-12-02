933153843032d81f2de442e84e820de7fff2b92d
Steven Ko <chunyuko85@gmail.com>
2013-12-02 14:09:43 +0800

    don't return not approved data

--- spec/controllers/api/get_share_payment_datas_controller_spec.rb
+++ spec/controllers/api/get_share_payment_datas_controller_spec.rb
@@ -26,14 +26,24 @@ describe Api::V1::GetSharePaymentDatasController do
 
   describe "Get users_who_shared_by_owner" do
 
-    it "return all user shared payments" do
+    it "return all user shared payments which is approved" do
+      bob = Fabricate(:user)
+      john = Fabricate(:user)
+      share_payment = Fabricate(:payment, user_id: bob.id)
+      relation = Fabricate(:user_share_payment_relation, share_user_id: john.id, owner_user_id: bob.id, payment_hash_key: share_payment.hash_key, token: SecureRandom.urlsafe_base64, is_approved: true)
+      get :users_who_shared_by_owner, {owner_user: bob.email, payment_hash_key: share_payment.hash_key}
+      body = ActiveSupport::JSON.decode(response.body)
+      expect(body.size).to eq(1)
+    end
+
+    it "don't return all user shared payments which is not approved" do
       bob = Fabricate(:user)
       john = Fabricate(:user)
       share_payment = Fabricate(:payment, user_id: bob.id)
       relation = Fabricate(:user_share_payment_relation, share_user_id: john.id, owner_user_id: bob.id, payment_hash_key: share_payment.hash_key, token: SecureRandom.urlsafe_base64)
       get :users_who_shared_by_owner, {owner_user: bob.email, payment_hash_key: share_payment.hash_key}
       body = ActiveSupport::JSON.decode(response.body)
-      expect(body.size).to eq(1)
+      expect(body.size).to eq(0)
     end
 
     context "with invalid user data" do
