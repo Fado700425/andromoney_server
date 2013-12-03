7e7d46657246bde4b5eec8eba98528e09a038d9f
Steven Ko <chunyuko85@gmail.com>
2013-12-03 14:38:37 +0800

    fix relation payment method error

--- app/models/user_share_payment_relation.rb
+++ app/models/user_share_payment_relation.rb
@@ -1,7 +1,11 @@
 class UserSharePaymentRelation < ActiveRecord::Base
   belongs_to :owner, foreign_key: :owner_user_id, class_name: "User"
   belongs_to :share, foreign_key: :share_user_id, class_name: "User"
-  belongs_to :payment, foreign_key: :payment_hash_key, class_name: "Payment", primary_key: :hash_key
+  # belongs_to :payment, foreign_key: :payment_hash_key, class_name: "Payment", primary_key: :hash_key
 
   validates_uniqueness_of :payment_hash_key, scope: [ :owner_user_id, :share_user_id ]
+
+  def payment
+    Payment.find_by(hash_key: payment_hash_key, user_id: owner_user_id)
+  end
 end
