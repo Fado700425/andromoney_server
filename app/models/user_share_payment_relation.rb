f265123a9969a9c7eb03135f69054e8a967e01c3
Steven Ko <chunyuko85@gmail.com>
2013-12-03 14:25:55 +0800

    validate uniqueness of payment_hash_key, scope: [ :owner_user_id, :share_user_id ]

--- app/models/user_share_payment_relation.rb
+++ app/models/user_share_payment_relation.rb
@@ -2,4 +2,6 @@ class UserSharePaymentRelation < ActiveRecord::Base
   belongs_to :owner, foreign_key: :owner_user_id, class_name: "User"
   belongs_to :share, foreign_key: :share_user_id, class_name: "User"
   belongs_to :payment, foreign_key: :payment_hash_key, class_name: "Payment", primary_key: :hash_key
+
+  validates_uniqueness_of :payment_hash_key, scope: [ :owner_user_id, :share_user_id ]
 end
