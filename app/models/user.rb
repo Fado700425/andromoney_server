a3e287959ab988aa91bf8753c923e3603a63ff68
Steven Ko <chunyuko85@gmail.com>
2014-01-02 22:33:59 +0800

    rm validate uid for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,4 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_uniqueness_of :email
 
-  validates_uniqueness_of :uid, :scope => :provider
 end
