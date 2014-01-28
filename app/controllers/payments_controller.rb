ccd822329a6176db873347eb2f1ec6c5a13355bc
Steven Ko <chunyuko85@gmail.com>
2014-01-28 21:51:50 +0800

    fix error

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,10 +12,11 @@ class PaymentsController < ApplicationController
     init  = payment.init_record
 
     unless init
-      init = Record.new(in_payment: payment.hash_key, category: "SYSTEM", sub_category: "INIT_AMOUNT", user_id: current_user.id, currency_code: payment.currency_code, amount_to_main: 0, hash_key: SecureRandom.urlsafe_base64)
+      init = Record.new(in_payment: payment.hash_key, category: "SYSTEM", sub_category: "INIT_AMOUNT", user_id: current_user.id, currency_code: payment.currency_code, hash_key: SecureRandom.urlsafe_base64, device_uuid: "computer", date: DateTime.parse('1010-01-01 00:00:00'), update_time: Time.now)
     end
 
     init.mount = params[:initial_amount].to_f
+    init.amount_to_main = init.calculate_record_amount(current_user.get_main_currency)
     init.save
     
     payment.device_uuid = "computer"
@@ -37,8 +38,9 @@ class PaymentsController < ApplicationController
     payment.total = 0
     payment.hidden = 0
     payment.save
-    init_record = Record.create(in_payment: payment.hash_key, category: "SYSTEM", sub_category: "INIT_AMOUNT", user_id: current_user.id, currency_code: payment.currency_code, amount_to_main: 0, hash_key: SecureRandom.urlsafe_base64)
+    init_record = Record.create(in_payment: payment.hash_key, category: "SYSTEM", sub_category: "INIT_AMOUNT", user_id: current_user.id, currency_code: payment.currency_code, hash_key: SecureRandom.urlsafe_base64, device_uuid: "computer", date: DateTime.parse('1010-01-01 00:00:00'), update_time: Time.now)
     init_record.mount = params[:initial_amount].to_f
+    init_record.amount_to_main = init_record.calculate_record_amount(current_user.get_main_currency)
     init_record.save
 
     redirect_to payments_path
