c68732b15f2ae37ac83a259cd6d56d224b89ca5d
Steven Ko <chunyuko85@gmail.com>
2014-02-11 17:43:51 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
 require 'bundler/capistrano'
-# require 'capistrano/local_precompile'
+require 'capistrano/local_precompile'
 # require 'hoptoad_notifier/capistrano'
 
 set :application, "andromoney_server"
 set :rails_env, "production"
 
-set :branch, "master"
+set :branch, "web_feature"
 set :repository,  "https://github.com/StevenKo/andromoney_server.git"
 set :scm, "git"
 set :user, "apps" # 一個伺服器上的帳戶用來放你的應用程式，不需要有sudo權限，但是需要有權限可以讀取Git repository拿到原始碼
@@ -18,6 +18,9 @@ set :use_sudo, false
 # role :web, "106.186.122.183"
 # role :app, "106.186.122.183"
 # role :db,  "106.186.122.183", :primary => true
+
+set :bundle_cmd, "RAILS_ENV=production bundle"
+
 
 role :web, "106.186.22.13"
 role :app, "106.186.22.13"
@@ -41,31 +44,31 @@ end
 
 set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
  
-namespace :deploy do
-  namespace :assets do
+# namespace :deploy do
+#   namespace :assets do
  
-    desc <<-DESC
-      Run the asset precompilation rake task. You can specify the full path \
-      to the rake executable by setting the rake variable. You can also \
-      specify additional environment variables to pass to rake via the \
-      asset_env variable. The defaults are:
+#     desc <<-DESC
+#       Run the asset precompilation rake task. You can specify the full path \
+#       to the rake executable by setting the rake variable. You can also \
+#       specify additional environment variables to pass to rake via the \
+#       asset_env variable. The defaults are:
  
-        set :rake,      "rake"
-        set :rails_env, "production"
-        set :asset_env, "RAILS_GROUPS=assets"
-        set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
-    DESC
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
+#         set :rake,      "rake"
+#         set :rails_env, "production"
+#         set :asset_env, "RAILS_GROUPS=assets"
+#         set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
+#     DESC
+#     task :precompile, :roles => :web, :except => { :no_release => true } do
+#       from = source.next_revision(current_revision)
+#       if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+#         run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+#       else
+#         logger.info "Skipping asset pre-compilation because there were no asset changes"
+#       end
+#     end
  
-  end
-end
+#   end
+# end
 
 
 before "deploy:assets:precompile", "deploy:copy_config_files" # 如果將database.yml放在shared下，請打開
