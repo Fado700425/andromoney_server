d0e48ea9d8db02932abee937976c924a4a6aae37
Steven Ko <chunyuko85@gmail.com>
2014-02-19 20:18:34 +0800

    chart

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -44,11 +44,23 @@ class ReportsController < ApplicationController
 
       @trends_array << array
     end
+
+    colors = ['#fecb0a', '#a3c22b', '#319aa8', '#fd8711', '#fa373f', '#f351af' , '#358ad3', '#fa373f', '#f351af' , '#358ad3']
+    color_index = 0
+
     case params[:report][:type]
     when "payment"
-      payments = Payment.where(hash_key: payments)
-      @chart_array = [['帳戶', '結餘']]
-      payments.each do |payment|
+      payments = Payment.where(hash_key: payments).sort_by{|p| -p.balance.abs}
+
+      @chart_array = [['專案', '總計', { role: "style" }]]
+      @pie_array = [['帳戶', '結餘']]
+
+      other_array = []
+      other_array << '其它'
+      other_balance = 0
+
+      payments.each_with_index do |payment, index|
+
         array = []
         balance = payment.balance.to_f
         if balance > 0
@@ -58,44 +70,126 @@ class ReportsController < ApplicationController
         end
         array << balance.abs
 
-        @chart_array << array
+        if @chart_array.size < 10
+          @pie_array << array.dup
+          array << colors[index + color_index]
+          @chart_array << array
+        else
+          other_balance += balance
+        end
+      end
+      other_array << other_balance.abs
+      if @chart_array.size == 10
+        @pie_array << other_array.dup
+        other_array << colors[0]
+        @chart_array << other_array
       end
     when "project"
       @chart_array = [['專案', '總計']]
       project_amount = Record.month_from_now(params[:month_from_now].to_i).where("user_id = #{current_user.id}").group(:project).sum(:amount_to_main)
+      project_amount = project_amount.sort_by{|key,val| val}
+
+      other_array = []
+      other_array << '其它'
+      other_amount = 0
+
       project_amount.each do |hash_key, amount|
         if projects.include? hash_key
           array = []
           array << Project.find_by(hash_key: hash_key).category
           array << amount.to_f
 
-          @chart_array << array
+          if @chart_array.size < 10
+            @pie_array << array.dup
+            array << colors[index + color_index]
+            @chart_array << array
+          else
+            other_amount += amount.to_f
+          end
+        else
+          color_index -= 1
         end
       end
+      other_array << other_amount
+      if @chart_array.size == 10
+        @pie_array << other_array.dup
+        other_array << colors[0]
+        @chart_array << other_array
+      end
     when "payee"
-      @chart_array = [['專案', '總計']]
+      @chart_array = [['專案', '總計', { role: "style" }]]
+      @pie_array = [['專案', '總計']]
       payee_amount = Record.month_from_now(params[:month_from_now].to_i).where("user_id = #{current_user.id}").group(:payee).sum(:amount_to_main)
-      payee_amount.each do |hash_key, amount|
+      payee_amount = payee_amount.sort_by{|key,val| val}
+
+      other_array = []
+      other_array << '其它'
+      other_amount = 0
+
+      payee_amount.each_with_index do |hash_key, index|
+        hash_key = hash[0]
+        amount = hash[1]
+
         if payees.include? hash_key
           array = []
           array << Payee.find_by(hash_key: hash_key).category
           array << amount.to_f
-
-          @chart_array << array
+          
+          if @chart_array.size < 10
+            @pie_array << array.dup
+            array << colors[index + color_index]
+            @chart_array << array
+          else
+            other_amount += amount.to_f
+          end
+        else
+          color_index -= 1
         end
       end
+      other_array << other_amount
+      if @chart_array.size == 10
+        @pie_array << other_array.dup
+        other_array << colors[0]
+        @chart_array << other_array
+      end 
     when "category"
-      @chart_array = [['類別', '總計']]
+      @chart_array = [['類別', '總計', { role: "style" }]]
+      @pie_array = [['類別', '總計']]
       category_amount = Record.month_from_now(params[:month_from_now].to_i).where("user_id = #{current_user.id}").group(:category).sum(:amount_to_main)
-      category_amount.each do |hash_key, amount|
+      category_amount = category_amount.sort_by{|key,val| val}
+
+      other_array = []
+      other_array << '其它'
+      other_amount = 0
+
+      category_amount.each_with_index do |hash, index|
+        hash_key = hash[0]
+        amount = hash[1]
+
         if categories.include? hash_key
           array = []
           array << Category.find_by(hash_key: hash_key).category
           array << amount.to_f
 
-          @chart_array << array
+          if @chart_array.size < 10
+            @pie_array << array.dup
+            array << colors[index + color_index]
+            @chart_array << array
+          else
+            other_amount += amount.to_f
+          end
+        else
+          color_index -= 1
         end
       end
+      
+      other_array << other_amount
+      
+      if @chart_array.size == 10
+        @pie_array << other_array.dup
+        other_array << colors[0]
+        @chart_array << other_array
+      end 
     end
 
   end
