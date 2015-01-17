8f26c5003496c0822edd93d248cef65542d57ee4
Ted <ted@TeddeMacBook-Pro.local>
2015-01-17 15:29:44 +0800

    date must not be null

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,8 @@
 class Record < ActiveRecord::Base
   self.table_name = "record_table"
   belongs_to :user
-  validates_uniqueness_of :hash_key, scope: [ :user_id ]
+  validates_uniqueness_of :hash_key,  scope: [ :user_id ]
+  validates_presence_of :date
 
   scope :api_select, -> { where(is_delete: false).select("id,mount,category,sub_category,date, in_payment,out_payment,remark,currency_code,amount_to_main,period,
                               payee,project,fee,in_amount,out_amount,in_currency,out_currency,hash_key,update_time"
