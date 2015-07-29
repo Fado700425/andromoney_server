667f60bc704c1deda7f44691f642663c0c8877ff
Ted <ted@host-127.145-43-115.dynamic.totalbb.net.tw>
2015-07-29 23:42:12 +0800

    Fix UTF8 to UTF8mb4 sql

--- db/migrate/20150727141101_update_utf8_text_to_utf8mb4.rb
+++ db/migrate/20150727141101_update_utf8_text_to_utf8mb4.rb
@@ -1,10 +1,11 @@
 class UpdateUtf8TextToUtf8mb4 < ActiveRecord::Migration
   def change
-    execute "ALTER TABLE record_table CHANGE remark remark TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    execute "ALTER TABLE record_table charset=utf8mb4, MODIFY COLUMN remark text CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci;"
     execute "ALTER TABLE category_table charset=utf8mb4, MODIFY COLUMN category varchar(255) CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci;"
     execute "ALTER TABLE payment_table charset=utf8mb4, MODIFY COLUMN payment_name VARCHAR(255) CHARACTER SET utf8mb4,MODIFY COLUMN remark VARCHAR(255) CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci;"
     execute "ALTER TABLE project_table charset=utf8mb4, MODIFY COLUMN project_name varchar(255) CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci;"
     execute "ALTER TABLE subcategory_table charset=utf8mb4, MODIFY COLUMN subcategory varchar(255) CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci;"
     execute "ALTER TABLE payee_table charset=utf8mb4, MODIFY COLUMN payee_name varchar(255) CHARACTER SET utf8mb4  COLLATE utf8mb4_unicode_ci;"
+    execute "ALTER DATABASE andromoney CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;"
   end
 end
