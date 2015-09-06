5d7ef44d8d4db9fa0fecf16e1c00258809f87d38
Lucas Yang <lucasmf@yahoo-inc.com>
2015-09-06 08:18:54 +0800

    add_sample_data_and_sample_user_for_dev

--- spec/fabricators/subcategory_fabricator.rb
+++ spec/fabricators/subcategory_fabricator.rb
@@ -1,8 +1,10 @@
 Fabricator(:subcategory) do
-  hash_key {Faker::Lorem.characters(20)}
+  transient user_id: nil
+  user { |attrs| attrs[:user_id].nil?? nil: User.find(attrs[:user_id]) }
+  hash_key { Faker::Lorem.characters(20) }
   is_delete false
-  device_uuid {Faker::Lorem.characters(20)}
-  subcategory {Faker::Lorem.characters(20)}
-  hidden 1
-  update_time Time.now
+  device_uuid { Faker::Lorem.characters(20) }
+  subcategory { Faker::Lorem.characters(20) }
+  hidden 0
+  update_time Time.zone.now
 end
