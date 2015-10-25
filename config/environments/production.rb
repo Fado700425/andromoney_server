f07913d57329365ad4d45f57ffe9910de032a7e2
Lucas Yang <lucasmf@yahoo-inc.com>
2015-10-26 06:15:21 +0800

    disable asset compress for debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,4 @@
+
 AndromoneyServer::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -25,9 +26,10 @@ AndromoneyServer::Application.configure do
   # Compress JavaScripts and CSS.
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.assets.compress = true
-  config.assets.js_compressor = NoCompression.new
-  config.assets.css_compressor = NoCompression.new
+  # config.assets.compress = true
+  # config.assets.js_compressor = NoCompression.new
+  # config.assets.css_compressor = NoCompression.new
+  config.assets.debug = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
