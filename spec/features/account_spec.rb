decec47ccb60c602a31c723e97968d9704e896eb
mozzan <linch0520@gmail.com>
2015-11-01 22:43:55 +0800

    add feature rspec

--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -1,25 +1,30 @@
 require 'spec_helper'
 
-feature "account", :omniauth, js: true do
+feature "account", :omniauth  do
   given(:john) { create_user('john@andromoney.com', 'John Doe', 1) }
 
   background do
     signin(john)
-    set_user_assest(john)
+    Fabricate(:record, user: john, mount: 10, currency_code: 'ARS', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
+    Fabricate(:record, user: john, mount: 100, currency_code: 'BDT', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
+    Fabricate(:record, user: john, mount: 500, currency_code: 'EUR', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
+    Fabricate(:record, user: john, mount: 2000, currency_code: 'JMD', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
+    Fabricate(:record, user: john, mount: 5000, currency_code: 'VND', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
+    Fabricate(:record, user: john, mount: 10000, currency_code: 'YER', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
+    Fabricate(:record, user: john, mount: 20000, currency_code: 'TWD', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour) 
   end
 
-  before(:all) do
-    Fabricate(:record, user_id: john.id, mount: 10, currency_code: 'ARS', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
-    Fabricate(:record, user_id: john.id, mount: 100, currency_code: 'BDT', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
-    Fabricate(:record, user_id: john.id, mount: 500, currency_code: 'EUR', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
-    Fabricate(:record, user_id: john.id, mount: 2000, currency_code: 'JMD', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
-    Fabricate(:record, user_id: john.id, mount: 5000, currency_code: 'VND', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
-    Fabricate(:record, user_id: john.id, mount: 10000, currency_code: 'YER', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
-    Fabricate(:record, user_id: john.id, mount: 20000, currency_code: 'TWD', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour) 
-  end
-
-  describe 'New page' do
+  describe 'test main currency switch' do
     it "update to USD" do
+      visit '/accounts/main_currency'
+      #get 'accounts/main_currency'
+      #post 'setting/update', selected_currency: 'USD'
+      #expect(Currency.where('user_id = ? AND sequence_status = ?', session[:user_id], 0)[0].currency_code).to eq('USD')
+      #expect(flash[:success]).to be_present
+
+      #get 'setting/edit'
+      #post 'setting/update', selected_currency: 'TWD'
+      #expect(flash[:danger]).to be_present
     end
   end
 end
