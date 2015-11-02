281be52cb0d4cfadd1580cb9d64fc05d0cdacd75
mozzan <linch0520@gmail.com>
2015-11-02 10:56:54 +0800

    add currencies, add update main currency spec

--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
-feature "account", :omniauth  do
+feature "account", :omniauth, js: true  do
   given(:john) { create_user('john@andromoney.com', 'John Doe', 1) }
 
   background do
     signin(john)
+    set_user_assest(john)
     Fabricate(:record, user: john, mount: 10, currency_code: 'ARS', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
     Fabricate(:record, user: john, mount: 100, currency_code: 'BDT', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
     Fabricate(:record, user: john, mount: 500, currency_code: 'EUR', update_time: Time.now - 1.hour, updated_at: Time.now - 1.hour, date: Time.now - 1.hour)
@@ -15,16 +16,22 @@ feature "account", :omniauth  do
   end
 
   describe 'test main currency switch' do
-    it "update to USD" do
-      visit '/accounts/main_currency'
-      #get 'accounts/main_currency'
-      #post 'setting/update', selected_currency: 'USD'
-      #expect(Currency.where('user_id = ? AND sequence_status = ?', session[:user_id], 0)[0].currency_code).to eq('USD')
-      #expect(flash[:success]).to be_present
+    before { visit '/accounts/main_currency' }
 
-      #get 'setting/edit'
-      #post 'setting/update', selected_currency: 'TWD'
-      #expect(flash[:danger]).to be_present
+    scenario "update to USD" do
+      find("#search_box").set 'u'
+      find('.flag_america').click
+      click_button I18n.t('confirm_edit')
+      expect(Currency.find_by('user_id = ? AND sequence_status = ?', john.id, 0).currency_code).to eq('USD')
     end
+  end
+
+  after(:each) do
+    # check record's amount_to_main
+    mainCurrencyRate = Currency.find_by('user_id = ? AND sequence_status = ?', john.id, 0).rate
+    Record.where('user_id = ?', john.id).each { |record|
+      rate = Currency.find_by("currency_code = ? AND user_id = ?", record.currency_code, john.id).rate
+      expect((((record.mount / rate * mainCurrencyRate) * 100)).round / 100.0).to eq(record.amount_to_main.to_f)
+    }
   end
 end
