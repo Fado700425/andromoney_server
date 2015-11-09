d90d6007c0225ff7b62abdac0be3531f71daaecb
Daryl Chen <otaq.hsc@gmail.com>
2015-11-09 22:54:24 +0800

    Fix rspec deprecation warnings
    Deprecation Warnings:
    
    RSpec::Core::Configuration#treat_symbols_as_metadata_keys_with_true_values= is deprecated, it is now set to true as default and setting it to false has no effect.
    
    Requiring `rspec/autorun` when running RSpec via the `rspec` command is deprecated. Called from /home/ubuntu/andromoney_server/vendor/bundle/ruby/2.1.0/gems/activesupport-4.0.0/lib/active_support/dependencies.rb:228:in `require'.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 
 require 'rspec/rails'
-require 'rspec/autorun'
 
 require 'capybara/rails'
 require 'capybara/rspec'
@@ -40,8 +39,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "random"
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  
+
   # omniauth
   config.include Omniauth::Mock
   config.include Omniauth::SessionHelpers, type: :feature
